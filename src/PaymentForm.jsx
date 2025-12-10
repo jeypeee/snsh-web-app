98cc8828a9368400f79be27b59bbdde871004055
jeypeee <jpdalay@gmail.com>
2025-12-10 10:37:16 +0800

    Small updates

---
+++ src/PaymentForm.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+
+function PaymentForm() {
+  return (
+    <form>
+      <label htmlFor="grade">Grade</label>
+      <select name="grade" id="grade">
+        <option value="grade-7">Grade 7</option>
+        <option value="grade-8">Grade 8</option>
+        <option value="grade-9">Grade 9</option>
+        <option value="grade-10">Grade 10</option>
+      </select>
+
+      <label htmlFor="section">Section</label>
+      <select name="section" id="section">
+        <option value="section-a">Section A</option>
+        <option value="section-a">Section B</option>
+        <option value="section-a">Section C</option>
+      </select>
+
+      <label htmlFor="name">Name</label>
+      <input type="text" />
+
+      <label>
+        Payment Amount:
+        <input type="text" />
+      </label>
+      <label>
+        Payment Date:
+        <input type="text" />
+      </label>
+    </form>
+  );
+}
+
+export default PaymentForm;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppSample from "./AppSample.jsx";
+import PaymentForm from "./PaymentForm.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PaymentForm />
+    <AppSample />
+  </StrictMode>
+);
